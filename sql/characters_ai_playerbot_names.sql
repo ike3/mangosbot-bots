17627942548d7a07c3a262d972081beebb0f6a1a
ConanHUN <conan_513@hotmail.com>
2018-10-26 11:29:29 +0200

    Update sql/characters_ai_playerbot_names.sql

--- sql/characters_ai_playerbot_names.sql
+++ sql/characters_ai_playerbot_names.sql
@@ -6,7 +6,7 @@ CREATE TABLE `ai_playerbot_names` (
 PRIMARY KEY (`name_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='PlayerbotAI RandomBot names';
 
-LOAD DATA LOCAL INFILE 'c:/tmp/characters_ai_playerbot_names.csv' INTO TABLE ai_playerbot_names FIELDS TERMINATED BY ';';
+LOAD DATA LOCAL INFILE './characters_ai_playerbot_names.csv' INTO TABLE ai_playerbot_names FIELDS TERMINATED BY ';';
 
 delete FROM ai_playerbot_names  where length(name) > 12;
 alter table ai_playerbot_names modify name varchar(12);
