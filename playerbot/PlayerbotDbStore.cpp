6584a52bd8416f9cb861ac4a38daac137c32fddf
apock69 <35844124+apock69@users.noreply.github.com>
2019-02-28 19:02:29 -0800

    Update PlayerbotDbStore.cpp
    Compiler wants strict formating for types.

--- playerbot/PlayerbotDbStore.cpp
+++ playerbot/PlayerbotDbStore.cpp
@@ -21,7 +21,7 @@ void PlayerbotDbStore::Load(PlayerbotAI *ai)
     if (sPlayerbotAIConfig.IsInRandomAccountList(account))
         return;
 
-    QueryResult* results = CharacterDatabase.PQuery("SELECT `key`,`value` FROM `ai_playerbot_db_store` WHERE `guid` = '%u'", guid);
+    QueryResult* results = CharacterDatabase.PQuery("SELECT `key`,`value` FROM `ai_playerbot_db_store` WHERE `guid` = '%lu'", guid);
     if (results)
     {
         ai->ClearStrategies(BOT_STATE_COMBAT);
@@ -116,10 +116,10 @@ void PlayerbotDbStore::Reset(PlayerbotAI *ai)
     if (sPlayerbotAIConfig.IsInRandomAccountList(account))
         return;
 
-    CharacterDatabase.PExecute("DELETE FROM `ai_playerbot_db_store` WHERE `guid` = '%u'", guid);
+    CharacterDatabase.PExecute("DELETE FROM `ai_playerbot_db_store` WHERE `guid` = '%lu'", guid);
 }
 
 void PlayerbotDbStore::SaveValue(uint64 guid, string key, string value)
 {
-    CharacterDatabase.PExecute("INSERT INTO `ai_playerbot_db_store` (`guid`, `key`, `value`) VALUES ('%u', '%s', '%s')", guid, key.c_str(), value.c_str());
+    CharacterDatabase.PExecute("INSERT INTO `ai_playerbot_db_store` (`guid`, `key`, `value`) VALUES ('%lu', '%s', '%s')", guid, key.c_str(), value.c_str());
 }
