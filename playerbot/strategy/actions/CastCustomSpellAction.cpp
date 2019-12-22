d397d2c8a53d08a30f77f6655ddf4be3849efbc6
ike3 <ike3@server.org>
2019-12-22 15:26:12 +0300

    [PLAYERBOTS] clear motion master before casting any custom spell

--- playerbot/strategy/actions/CastCustomSpellAction.cpp
+++ playerbot/strategy/actions/CastCustomSpellAction.cpp
@@ -90,6 +90,9 @@ bool CastCustomSpellAction::Execute(Event event)
         return false;
     }
 
+    MotionMaster &mm = *bot->GetMotionMaster();
+    mm.Clear();
+
     bool result = spell ? ai->CastSpell(spell, target, itemTarget) : ai->CastSpell(text, target, itemTarget);
     if (result)
     {
