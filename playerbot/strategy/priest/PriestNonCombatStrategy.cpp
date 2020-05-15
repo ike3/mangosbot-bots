29bd4aba0f33bbafa77efd3f14695f3eaa25c985
ike3 <ike3@server.org>
2020-05-15 19:14:45 +0300

    [PLAYERBOTS] Remove non-combat healing

--- playerbot/strategy/priest/PriestNonCombatStrategy.cpp
+++ playerbot/strategy/priest/PriestNonCombatStrategy.cpp
@@ -29,47 +29,9 @@ void PriestNonCombatStrategy::InitTriggers(std::list<TriggerNode*> &triggers)
         NextAction::array(0, new NextAction("inner fire", 10.0f), NULL)));
 
 
-    triggers.push_back(new TriggerNode(
-        "critical health",
-        NextAction::array(0, new NextAction("power word: shield", 70.0f), new NextAction("greater heal", 70.0f), NULL)));
-
-    triggers.push_back(new TriggerNode(
-        "party member critical health",
-        NextAction::array(0, new NextAction("power word: shield on party", 60.0f), new NextAction("greater heal on party", 60.0f), NULL)));
-
-    triggers.push_back(new TriggerNode(
-        "low health",
-        NextAction::array(0, new NextAction("flash heal", 21.0f), NULL)));
-
-    triggers.push_back(new TriggerNode(
-        "party member low health",
-        NextAction::array(0, new NextAction("flash heal on party", 20.0f), NULL)));
-
-    triggers.push_back(new TriggerNode(
-        "medium aoe heal",
-        NextAction::array(0, new NextAction("circle of healing", 27.0f), NULL)));
-
 	triggers.push_back(new TriggerNode(
 		"party member dead",
 		NextAction::array(0, new NextAction("resurrection", 30.0f), NULL)));
-
-
-    triggers.push_back(new TriggerNode(
-        "dispel magic",
-        NextAction::array(0, new NextAction("dispel magic", 41.0f), NULL)));
-
-    triggers.push_back(new TriggerNode(
-        "dispel magic on party",
-        NextAction::array(0, new NextAction("dispel magic on party", 40.0f), NULL)));
-
-
-    triggers.push_back(new TriggerNode(
-        "cure disease",
-        NextAction::array(0, new NextAction("abolish disease", 31.0f), NULL)));
-
-    triggers.push_back(new TriggerNode(
-        "party member cure disease",
-        NextAction::array(0, new NextAction("abolish disease on party", 30.0f), NULL)));
 }
 
 void PriestBuffStrategy::InitTriggers(std::list<TriggerNode*> &triggers)
