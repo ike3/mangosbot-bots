c9aa3843f00214c7cb827cb3446451234fc19dcf
celguar <celguar@gmail.com>
2020-07-04 11:53:25 +0300

    Temporary disable rogue poisons (possible crash)

--- playerbot/strategy/rogue/GenericRogueNonCombatStrategy.cpp
+++ playerbot/strategy/rogue/GenericRogueNonCombatStrategy.cpp
@@ -9,11 +9,12 @@ using namespace ai;
 
 void GenericRogueNonCombatStrategy::InitTriggers(std::list<TriggerNode*> &triggers)
 {
-    NonCombatStrategy::InitTriggers(triggers);
-    
-    triggers.push_back(new TriggerNode(
-       "often",
-       NextAction::array(0, new NextAction("apply poison", 1.0f), NULL)));
+	//TODO Possible crash on CMSG_USE_ITEM ByteBuffer error
+    //NonCombatStrategy::InitTriggers(triggers);
+    //
+    //triggers.push_back(new TriggerNode(
+    //   "often",
+    //   NextAction::array(0, new NextAction("apply poison", 1.0f), NULL)));
 
 
 }
