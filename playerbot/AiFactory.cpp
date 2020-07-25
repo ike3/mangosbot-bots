fdb60c2f2e67f901c004e65224899386905398b0
ike3 <ike3@server.org>
2020-07-25 13:49:25 +0300

    frost mage: change blizzard to medium aoe

--- playerbot/AiFactory.cpp
+++ playerbot/AiFactory.cpp
@@ -161,11 +161,11 @@ void AiFactory::AddDefaultCombatStrategies(Player* player, PlayerbotAI* const fa
             break;
         case CLASS_PALADIN:
             if (tab == 1)
-                engine->addStrategies("tank", "tank aoe", "bthreat", "cure", NULL);
+                engine->addStrategies("tank", "tank aoe", "bthreat", "cure", "baoe", NULL);
 			else if(tab == 0)
 				engine->addStrategies("heal", "bmana", "cure", "flee", NULL);
             else
-                engine->addStrategies("dps", "bdps", "threat", "dps assist", "cure", NULL);
+                engine->addStrategies("dps", "bdps", "dps assist", "cure", NULL);
             break;
         case CLASS_DRUID:
             if (tab == 0)
