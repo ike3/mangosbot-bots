2aba8667928b54ea7f93cd14a70ed6f9e6a68a8a
ike3 <ike3@server.org>
2020-08-20 10:39:05 +0300

    [PLAYREBOTS] 'dps aoe' strategy to evenly distribute dps among all targets

--- playerbot/strategy/generic/DpsAssistStrategy.cpp
+++ playerbot/strategy/generic/DpsAssistStrategy.cpp
@@ -11,5 +11,12 @@ void DpsAssistStrategy::InitTriggers(std::list<TriggerNode*> &triggers)
         NextAction::array(0, new NextAction("dps assist", 50.0f), NULL)));
 }
 
+void DpsAoeStrategy::InitTriggers(std::list<TriggerNode*> &triggers)
+{
+    triggers.push_back(new TriggerNode(
+        "not dps aoe target active",
+        NextAction::array(0, new NextAction("dps aoe", 50.0f), NULL)));
+}
+
 
 
--- playerbot/strategy/values/DpsTargetValue.cpp
+++ playerbot/strategy/values/DpsTargetValue.cpp
@@ -38,3 +38,38 @@ Unit* DpsTargetValue::Calculate()
     FindLeastHpTargetStrategy strategy(ai);
     return TargetValue::FindTarget(&strategy);
 }
+
+class FindMaxHpTargetStrategy : public FindTargetStrategy
+{
+public:
+    FindMaxHpTargetStrategy(PlayerbotAI* ai) : FindTargetStrategy(ai)
+    {
+        maxHealth = 0;
+    }
+
+public:
+    virtual void CheckAttacker(Unit* attacker, ThreatManager* threatManager)
+    {
+        Group* group = ai->GetBot()->GetGroup();
+        if (group)
+        {
+            uint64 guid = group->GetTargetIcon(4);
+            if (guid && attacker->GetObjectGuid() == ObjectGuid(guid))
+                return;
+        }
+        if (!result || result->GetHealth() < attacker->GetHealth())
+            result = attacker;
+    }
+
+protected:
+    float maxHealth;
+};
+
+Unit* DpsAoeTargetValue::Calculate()
+{
+    Unit* rti = RtiTargetValue::Calculate();
+    if (rti) return rti;
+
+    FindMaxHpTargetStrategy strategy(ai);
+    return TargetValue::FindTarget(&strategy);
+}
