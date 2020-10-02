49f52231a0d312027c57ca9a937a4f6a0815d3b5
celguar <celguar@gmail.com>
2020-10-02 15:22:37 +0300

    Add Flying mounts support

--- playerbot/strategy/actions/CheckMountStateAction.cpp
+++ playerbot/strategy/actions/CheckMountStateAction.cpp
@@ -118,8 +118,8 @@ bool CheckMountStateAction::Execute(Event event)
 
 bool CheckMountStateAction::Mount()
 {
-	Player* master = GetMaster();
-	ai->RemoveShapeshift();
+    Player* master = GetMaster();
+    ai->RemoveShapeshift();
 
     int32 masterSpeed = 150;
     const SpellEntry *masterSpell = NULL;
@@ -149,54 +149,54 @@ bool CheckMountStateAction::Mount()
         }
     }
 
-	map<int32, vector<uint32> > spells;
-	for (PlayerSpellMap::iterator itr = bot->GetSpellMap().begin(); itr != bot->GetSpellMap().end(); ++itr)
-	{
-		uint32 spellId = itr->first;
-		if (itr->second.state == PLAYERSPELL_REMOVED || itr->second.disabled || IsPassiveSpell(spellId))
-			continue;
+    //map<int32, vector<uint32> > spells;
+    map<uint32, map<int32, vector<uint32> > > allSpells;
+    for (PlayerSpellMap::iterator itr = bot->GetSpellMap().begin(); itr != bot->GetSpellMap().end(); ++itr)
+    {
+        uint32 spellId = itr->first;
+        if (itr->second.state == PLAYERSPELL_REMOVED || itr->second.disabled || IsPassiveSpell(spellId))
+            continue;
 
-		const SpellEntry* spellInfo = sServerFacade.LookupSpellInfo(spellId);
-		if (!spellInfo || spellInfo->EffectApplyAuraName[0] != SPELL_AURA_MOUNTED)
-			continue;
+        const SpellEntry* spellInfo = sServerFacade.LookupSpellInfo(spellId);
+        if (!spellInfo || spellInfo->EffectApplyAuraName[0] != SPELL_AURA_MOUNTED)
+            continue;
 
-		int32 effect = max(spellInfo->EffectBasePoints[1], spellInfo->EffectBasePoints[2]);
-		if (effect < masterSpeed)
-			continue;
+        int32 effect = max(spellInfo->EffectBasePoints[1], spellInfo->EffectBasePoints[2]);
+        if (effect < masterSpeed)
+            continue;
 
-		spells[effect].push_back(spellId);
-	}
-
+        uint32 index = 0;
 #ifndef MANGOSBOT_ZERO
+        index = (spellInfo->EffectApplyAuraName[1] == SPELL_AURA_MOD_FLIGHT_SPEED_MOUNTED ||
+            spellInfo->EffectApplyAuraName[2] == SPELL_AURA_MOD_FLIGHT_SPEED_MOUNTED) ? 1 : 0;
+#endif
+
+        allSpells[index][effect].push_back(spellId);
+    }
+
+
     int masterMountType = 0;
+#ifndef MANGOSBOT_ZERO
     if (masterSpell != NULL)
     {
-        masterMountType = (masterSpell->EffectApplyAuraName[1] == SPELL_AURA_MOD_INCREASE_MOUNTED_FLIGHT_SPEED ||
-            masterSpell->EffectApplyAuraName[2] == SPELL_AURA_MOD_INCREASE_MOUNTED_FLIGHT_SPEED) ? 1 : 0;
+        masterMountType = (masterSpell->EffectApplyAuraName[1] == SPELL_AURA_MOD_FLIGHT_SPEED_MOUNTED ||
+            masterSpell->EffectApplyAuraName[2] == SPELL_AURA_MOD_FLIGHT_SPEED_MOUNTED) ? 1 : 0;
     }
 #endif
 
-	for (map<int32, vector<uint32> >::iterator i = spells.begin(); i != spells.end(); ++i)
-	{
-		vector<uint32>& ids = i->second;
-		int index = urand(0, ids.size() - 1);
-		if (index >= ids.size())
-			continue;
+    map<int32, vector<uint32> >& spells = allSpells[masterMountType];
+
+    for (map<int32, vector<uint32> >::iterator i = spells.begin(); i != spells.end(); ++i)
+    {
+        vector<uint32>& ids = i->second;
+        int index = urand(0, ids.size() - 1);
+        if (index >= ids.size())
+            continue;
 
         MotionMaster &mm = *bot->GetMotionMaster();
-        
-        //bot->GetMotionMaster()->MovementExpired();
-        //bot->StopMoving(true);
-        //bot->SetRoot(true);
-        //ai->SetNextCheckDelay(3100);
-        //bot->StopMoving();
-        //ai->SetNextCheckDelay(3000);
-		ai->CastSpell(ids[index], bot);
-        
-        
-        //bot->SetRoot(false);
-		return true;
-	}
+        ai->CastSpell(ids[index], bot);
+        return true;
+    }
 
     list<Item*> items = AI_VALUE2(list<Item*>, "inventory items", "mount");
     if (!items.empty()) return UseItemAuto(*items.begin());
