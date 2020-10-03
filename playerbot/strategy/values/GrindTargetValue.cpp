71c372d433d74eff6705fef89f8070480be3e69c
celguar <celguar@gmail.com>
2020-10-03 21:55:09 +0300

    Revert GrindTargetValue change for BG

--- playerbot/strategy/values/GrindTargetValue.cpp
+++ playerbot/strategy/values/GrindTargetValue.cpp
@@ -66,8 +66,8 @@ Unit* GrindTargetValue::FindTargetForGrinding(int assistCount)
 		if ((int)unit->getLevel() - (int)bot->getLevel() > 4 && !unit->GetObjectGuid().IsPlayer())
 		    continue;
 
-        if (bot->InBattleGround() && bot->GetDistance(unit) > sPlayerbotAIConfig.farDistance)
-            continue;
+        //if (bot->InBattleGround() && bot->GetDistance(unit) > sPlayerbotAIConfig.spellDistance)
+            //continue;
 
 		Creature* creature = dynamic_cast<Creature*>(unit);
 		if (creature && creature->GetCreatureInfo() && creature->GetCreatureInfo()->Rank > CREATURE_ELITE_NORMAL)
