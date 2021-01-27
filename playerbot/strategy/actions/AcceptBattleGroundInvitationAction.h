de9030df54a12eb1d23237920aa169139f75d5e7
celguar <celguar@gmail.com>
2021-01-28 02:46:44 +0300

    comment AcceptBgInviteAction

--- playerbot/strategy/actions/AcceptBattleGroundInvitationAction.h
+++ playerbot/strategy/actions/AcceptBattleGroundInvitationAction.h
@@ -10,11 +10,11 @@ namespace ai
 
       virtual bool Execute(Event event)
       {
-
+         /*
          uint8 type = 0;                                             // arenatype if arena
          uint8 unk2;                                             // unk, can be 0x0 (may be if was invited?) and 0x1
          uint32 bgTypeId_ = BATTLEGROUND_WS;                                       // type id from dbc
-         uint16 unk = 0x1F90;                                              // 0x1F90 constant?*/
+         uint16 unk = 0x1F90;                                              // 0x1F90 constant?
          uint8 action = 1;
 
          WorldPacket packet(CMSG_BATTLEFIELD_PORT, 20);
@@ -22,7 +22,7 @@ namespace ai
          packet << bgTypeId_ << action;
          //bot->GetSession()->HandleBattleFieldPortOpcode(packet);
 
-         ai->ResetStrategies();
+         ai->ResetStrategies();*/
          return true;
       }
    };
