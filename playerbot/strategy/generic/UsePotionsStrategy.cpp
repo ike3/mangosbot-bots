977ec455ab53463908229605259bc74e0a555c98
celguar <celguar@gmail.com>
2021-03-25 22:16:56 +0300

    Use healthstone instead of potion if available

--- playerbot/strategy/generic/UsePotionsStrategy.cpp
+++ playerbot/strategy/generic/UsePotionsStrategy.cpp
@@ -4,13 +4,35 @@
 
 using namespace ai;
 
+class UsePotionsStrategyActionNodeFactory : public NamedObjectFactory<ActionNode>
+{
+public:
+    UsePotionsStrategyActionNodeFactory()
+    {
+        creators["healthstone"] = &healthstone;
+    }
+private:
+    static ActionNode* healthstone(PlayerbotAI* ai)
+    {
+        return new ActionNode("healthstone",
+            /*P*/ NULL,
+            /*A*/ NextAction::array(0, new NextAction("healing potion"), NULL),
+            /*C*/ NULL);
+    }
+};
+
+UsePotionsStrategy::UsePotionsStrategy(PlayerbotAI* ai) : Strategy(ai)
+{
+    actionNodeFactories.Add(new UsePotionsStrategyActionNodeFactory());
+}
+
 void UsePotionsStrategy::InitTriggers(std::list<TriggerNode*> &triggers)
 {
     Strategy::InitTriggers(triggers);
 
     triggers.push_back(new TriggerNode(
         "critical health",
-        NextAction::array(0, new NextAction("healing potion", ACTION_MEDIUM_HEAL + 1), NULL)));
+        NextAction::array(0, new NextAction("healthstone", ACTION_MEDIUM_HEAL + 1), NULL)));
 
     triggers.push_back(new TriggerNode(
         "low mana",
