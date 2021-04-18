7ae5512662db00675e5ce121969cde8b8afdc72d
celguar <celguar@gmail.com>
2021-04-19 02:49:43 +0300

    Make player master only if same group

--- playerbot/PlayerbotAI.cpp
+++ playerbot/PlayerbotAI.cpp
@@ -554,12 +554,12 @@ void PlayerbotAI::DoNextAction()
         {
             Player* member = gref->getSource();
             PlayerbotAI* ai = bot->GetPlayerbotAI();
-            if (member && member->IsInWorld() && (!master || !member->GetPlayerbotAI()) && (!master || master->GetPlayerbotAI() || (bot->InBattleGround() && !urand(0, 4))))
+            if (member && member->IsInWorld() && (member->IsInGroup(bot, true) && !group->IsLeader(bot->GetObjectGuid())) && (!master || !member->GetPlayerbotAI()) && (!master || master->GetPlayerbotAI() || (bot->InBattleGround() && !urand(0, 4))))
             {
                 ai->SetMaster(member);
                 ai->ResetStrategies();
                 ai->ChangeStrategy("-rpg,-grind,-travel", BOT_STATE_NON_COMBAT);
-                if (sServerFacade.GetDistance2d(bot, member) < 50.0f)
+                if (sServerFacade.GetDistance2d(bot, member) < 50.0f && member->IsInGroup(bot, true))
                     ai->ChangeStrategy("+follow", BOT_STATE_NON_COMBAT);
 
                 ai->TellMaster("Hello, I follow you!");
