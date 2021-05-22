0f198943e7c9a123f7b95f4393287f093aef1a29
celguar <celguar@gmail.com>
2021-05-23 00:32:52 +0300

    Do not mount early before BG start

--- playerbot/strategy/actions/CheckMountStateAction.cpp
+++ playerbot/strategy/actions/CheckMountStateAction.cpp
@@ -107,7 +107,7 @@ bool CheckMountStateAction::Execute(Event event)
     if (!bot->IsMounted() && (fartarget || chasedistance))
         return Mount();
 
-    if (attackdistance && bot->IsMounted())
+    if (attackdistance && bot->IsMounted() && (!noattackers && sServerFacade.IsInCombat(bot)))
     {
         WorldPacket emptyPacket;
         bot->GetSession()->HandleCancelMountAuraOpcode(emptyPacket);
@@ -152,6 +152,17 @@ bool CheckMountStateAction::isUseful()
     if (!firstmount)
         return false;
 
+    // Only mount if BG starts in less than 30 sec
+    if (bot->InBattleGround())
+    {
+        BattleGround *bg = bot->GetBattleGround();
+        if (bg && bg->GetStatus() == STATUS_WAIT_JOIN)
+        {
+            if (bg->GetStartDelayTime() > BG_START_DELAY_30S)
+                return false;
+        }
+    }
+
     return true;
 }
 
@@ -159,9 +170,9 @@ bool CheckMountStateAction::Mount()
 {
     if (sServerFacade.isMoving(bot))
     {
-        bot->GetMotionMaster()->Clear(true);
-        bot->InterruptMoving(1);
-        bot->GetMotionMaster()->MovementExpired();
+        bot->StopMoving();
+        bot->GetMotionMaster()->Clear();
+        bot->GetMotionMaster()->MoveIdle();
     }
 
     Player* master = GetMaster();
