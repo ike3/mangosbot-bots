42772ad77233f2786553eb2c4566f62ec57cce0c
celguar <celguar@gmail.com>
2021-06-03 23:55:48 +0300

    temporary fix for arena crash

--- playerbot/strategy/actions/MovementActions.cpp
+++ playerbot/strategy/actions/MovementActions.cpp
@@ -147,7 +147,8 @@ bool MovementAction::MoveTo(uint32 mapId, float x, float y, float z, bool idle, 
     if (generatePath)
     {
         z += CONTACT_DISTANCE;
-        bot->UpdateAllowedPositionZ(x, y, z);
+        if (!bot->InBattleGround())
+            bot->UpdateAllowedPositionZ(x, y, z);
     }    
 
     if (!IsMovingAllowed() && sServerFacade.UnitIsDead(bot))
@@ -918,7 +919,9 @@ bool SetBehindTargetAction::Execute(Event event)
     float x = target->GetPositionX() + cos(angle) * distance,
         y = target->GetPositionY() + sin(angle) * distance,
         z = target->GetPositionZ();
-    bot->UpdateGroundPositionZ(x, y, z);
+
+    if (!bot->InBattleGround())
+        bot->UpdateGroundPositionZ(x, y, z);
 
     return MoveTo(bot->GetMapId(), x, y, z);
 }
