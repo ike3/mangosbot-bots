95c783079e32346f088fdc7ff332ab2c410d8ab3
Mostlikely <sebastiaan.keek@gmail.com>
2021-07-28 15:51:29 +0200

    -Rpg fix: Bots no longer 'chat' with any npc when at low durability but only low-level quest givers.

--- playerbot/strategy/actions/RpgAction.cpp
+++ playerbot/strategy/actions/RpgAction.cpp
@@ -73,11 +73,11 @@ bool RpgAction::Execute(Event event)
     uint32 dStatus = bot->GetSession()->getDialogStatus(bot, wo, DIALOG_STATUS_NONE);
 
 #ifdef MANGOSBOT_ZERO  
-    if (dStatus == DIALOG_STATUS_REWARD2 || dStatus == DIALOG_STATUS_REWARD_REP || dStatus == DIALOG_STATUS_AVAILABLE || (AI_VALUE(uint8, "durability") <= 20 || dStatus == DIALOG_STATUS_CHAT))
+    if (dStatus == DIALOG_STATUS_REWARD2 || dStatus == DIALOG_STATUS_REWARD_REP || dStatus == DIALOG_STATUS_AVAILABLE || (AI_VALUE(uint8, "durability") <= 20 && dStatus == DIALOG_STATUS_CHAT))
 #elif MANGOSBOT_ONE
-    if (dStatus == DIALOG_STATUS_REWARD2 || dStatus == DIALOG_STATUS_REWARD || dStatus == DIALOG_STATUS_REWARD_REP || dStatus == DIALOG_STATUS_AVAILABLE || (AI_VALUE(uint8, "durability") <= 20 || dStatus == DIALOG_STATUS_CHAT))
+    if (dStatus == DIALOG_STATUS_REWARD2 || dStatus == DIALOG_STATUS_REWARD || dStatus == DIALOG_STATUS_REWARD_REP || dStatus == DIALOG_STATUS_AVAILABLE || (AI_VALUE(uint8, "durability") <= 20 && dStatus == DIALOG_STATUS_CHAT))
 #else
-    if (dStatus == DIALOG_STATUS_REWARD2 || dStatus == DIALOG_STATUS_REWARD || dStatus == DIALOG_STATUS_REWARD_REP || dStatus == DIALOG_STATUS_AVAILABLE || (AI_VALUE(uint8, "durability") <= 20 || dStatus == DIALOG_STATUS_LOW_LEVEL_AVAILABLE))
+    if (dStatus == DIALOG_STATUS_REWARD2 || dStatus == DIALOG_STATUS_REWARD || dStatus == DIALOG_STATUS_REWARD_REP || dStatus == DIALOG_STATUS_AVAILABLE || (AI_VALUE(uint8, "durability") <= 20 && dStatus == DIALOG_STATUS_LOW_LEVEL_AVAILABLE))
 #endif    
         elements.push_back(&RpgAction::quest);
     if (unit)
