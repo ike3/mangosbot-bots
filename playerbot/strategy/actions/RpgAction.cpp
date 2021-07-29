977ab278999c7e87efc3d8cc79bd5b7616f64da1
celguar <celguar@gmail.com>
2021-07-29 23:29:37 +0300

    fix tbc build

--- playerbot/strategy/actions/RpgAction.cpp
+++ playerbot/strategy/actions/RpgAction.cpp
@@ -74,11 +74,13 @@ bool RpgAction::Execute(Event event)
 
 #ifdef MANGOSBOT_ZERO  
     if (dStatus == DIALOG_STATUS_REWARD2 || dStatus == DIALOG_STATUS_REWARD_REP || dStatus == DIALOG_STATUS_AVAILABLE || (AI_VALUE(uint8, "durability") <= 20 || dStatus == DIALOG_STATUS_CHAT))
-#elif MANGOSBOT_ONE
+#endif
+#ifdef MANGOSBOT_ONE
     if (dStatus == DIALOG_STATUS_REWARD2 || dStatus == DIALOG_STATUS_REWARD || dStatus == DIALOG_STATUS_REWARD_REP || dStatus == DIALOG_STATUS_AVAILABLE || (AI_VALUE(uint8, "durability") <= 20 || dStatus == DIALOG_STATUS_CHAT))
-#else
+#endif
+#ifdef MANGOSBOT_TWO
     if (dStatus == DIALOG_STATUS_REWARD2 || dStatus == DIALOG_STATUS_REWARD || dStatus == DIALOG_STATUS_REWARD_REP || dStatus == DIALOG_STATUS_AVAILABLE || (AI_VALUE(uint8, "durability") <= 20 || dStatus == DIALOG_STATUS_LOW_LEVEL_AVAILABLE))
-#endif    
+#endif  
         elements.push_back(&RpgAction::quest);
     if (unit)
     {
