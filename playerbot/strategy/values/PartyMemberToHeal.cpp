84838a70aae2e38d2d9cc8634a336f558717f9a0
celguar <celguar@gmail.com>
2021-08-12 01:58:59 +0300

    Increase healing radius

--- playerbot/strategy/values/PartyMemberToHeal.cpp
+++ playerbot/strategy/values/PartyMemberToHeal.cpp
@@ -114,7 +114,7 @@ bool PartyMemberToHeal::CanHealPet(Pet* pet)
 bool PartyMemberToHeal::Check(Unit* player)
 {
     return player && player != bot && player->GetMapId() == bot->GetMapId() &&
-        sServerFacade.GetDistance2d(bot, player) < (player->IsPlayer() && ai->IsTank((Player*)player)) ? 50.0f : sPlayerbotAIConfig.spellDistance;
+        sServerFacade.GetDistance2d(bot, player) < (player->IsPlayer() && ai->IsTank((Player*)player)) ? 50.0f : 40.0f;
 }
 
 Unit* PartyMemberToProtect::Calculate()
