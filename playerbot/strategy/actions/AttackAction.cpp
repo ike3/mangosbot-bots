0d1b0949650cb2ee557e0f794f36e59402b0f1f4
celguar <celguar@gmail.com>
2021-08-23 19:00:55 +0300

    Tweak attack action

--- playerbot/strategy/actions/AttackAction.cpp
+++ playerbot/strategy/actions/AttackAction.cpp
@@ -125,15 +125,10 @@ bool AttackAction::Attack(Unit* target)
     if (IsMovingAllowed() && !sServerFacade.IsInFront(bot, target, sPlayerbotAIConfig.sightDistance, CAST_ANGLE_IN_FRONT))
         sServerFacade.SetFacingTo(bot, target);
 
-    if (bot->Attack(target, !ai->IsRanged(bot) || sServerFacade.GetDistance2d(bot, target) <= sPlayerbotAIConfig.tooCloseDistance))
-    {
-        ai->ChangeEngine(BOT_STATE_COMBAT);
-        return true;
-    }
-    else
-        context->GetValue<Unit*>("current target")->Set(NULL);
+    bool attacked = bot->Attack(target, !ai->IsRanged(bot));
+    ai->ChangeEngine(BOT_STATE_COMBAT);
 
-    return false;
+    return attacked;
 }
 
 bool AttackDuelOpponentAction::isUseful()
