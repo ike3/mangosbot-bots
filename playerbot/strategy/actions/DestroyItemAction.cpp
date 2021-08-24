807537402453ecc0eefd688bfdae10faf9970787
Mostlikely <sebastiaan.keek@gmail.com>
2021-08-24 22:47:21 +0200

    -Bot inventory feature: Bots without an active player master will now destroy items when they have less than 10% bag space remaining. They will destroy quest items when they need money and destroy vendor/ah items otherwise.

--- playerbot/strategy/actions/DestroyItemAction.cpp
+++ playerbot/strategy/actions/DestroyItemAction.cpp
@@ -32,3 +32,42 @@ void DestroyItemAction::DestroyItem(FindItemVisitor* visitor)
         ai->TellMaster(out);
     }
 }
+
+bool SmartDestroyItemAction::Execute(Event event)
+{
+    uint8 bagSpace = AI_VALUE(uint8, "bag space");
+
+    if (bagSpace < 90)
+        return false;
+
+    vector<uint32> bestToDestroy = { ITEM_USAGE_NONE };
+
+    if (!AI_VALUE(bool, "can sell") && AI_VALUE(bool, "should get money"))
+        bestToDestroy.push_back(ITEM_USAGE_QUEST);
+    else
+    {
+        bestToDestroy.push_back(ITEM_USAGE_VENDOR);
+        bestToDestroy.push_back(ITEM_USAGE_AH);
+    }
+
+    for (auto& usage : bestToDestroy)
+    {
+
+        list<Item*> items = AI_VALUE2(list<Item*>, "inventory items", "quest");
+
+        items.reverse();
+
+        for (auto& item : items)
+        {
+            FindItemByIdVisitor visitor(item->GetProto()->ItemId);
+            DestroyItem(&visitor);
+
+            bagSpace = AI_VALUE(uint8, "bag space");
+
+            if(bagSpace < 90)
+                return true;
+        }
+    }
+
+    return false;
+}
