8ab0bc4fb50a145b9a4494ff7439920b7c46c06c
ike3 <ike3@server.org>
2021-10-02 12:19:28 +0300

    [PLAYERBOTS] more aggressive druid healing

--- playerbot/strategy/druid/HealDruidStrategy.cpp
+++ playerbot/strategy/druid/HealDruidStrategy.cpp
@@ -40,6 +40,14 @@ void HealDruidStrategy::InitTriggers(std::list<TriggerNode*> &triggers)
         NextAction::array(0, new NextAction("regrowth on party", ACTION_MEDIUM_HEAL + 1), NULL)));
 
     triggers.push_back(new TriggerNode(
+        "low health",
+        NextAction::array(0, new NextAction("healing touch", ACTION_CRITICAL_HEAL + 2), NULL)));
+
+    triggers.push_back(new TriggerNode(
+        "party member low health",
+        NextAction::array(0,  new NextAction("healing touch on party", ACTION_CRITICAL_HEAL + 1), NULL)));
+
+    triggers.push_back(new TriggerNode(
         "almost full health",
         NextAction::array(0, new NextAction("rejuvenation", ACTION_LIGHT_HEAL + 2), NULL)));
 
