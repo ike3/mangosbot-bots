354d1719259c6cc2f3c465c06e77e66f352d1e46
Mostlikely <sebastiaan.keek@gmail.com>
2021-10-18 09:52:01 +0200

    -Performance: Remove internal camera on login to the server from sending unneeded update packages to bots. -Performance: When passing master packets only randombots in the players group are checked instead of 'all randombots'.

--- playerbot/PlayerbotMgr.cpp
+++ playerbot/PlayerbotMgr.cpp
@@ -142,6 +142,20 @@ Player* PlayerbotHolder::GetPlayerBot(uint64 playerGuid) const
     return (it == playerBots.end()) ? 0 : it->second;
 }
 
+class ViewPointAccess
+{
+public:
+    friend class Camera;
+
+    typedef std::list<Camera*> CameraList;
+
+    CameraList m_cameras;
+    GridType* m_grid;
+
+    void Attach(Camera* c) { m_cameras.push_back(c); }
+    void Detach(Camera* c) { m_cameras.remove(c); }
+};
+
 void PlayerbotHolder::OnBotLogin(Player * const bot)
 {
 	PlayerbotAI* ai = new PlayerbotAI(bot);
@@ -150,7 +164,17 @@ void PlayerbotHolder::OnBotLogin(Player * const bot)
 
     playerBots[bot->GetObjectGuid().GetRawValue()] = bot;
 
+
     Player* master = ai->GetMaster();
+
+    if (master != bot)
+    {
+        ViewPoint* view = &bot->GetViewPoint();
+        ViewPointAccess* viewAccess = reinterpret_cast<ViewPointAccess*>(view);
+
+        viewAccess->m_cameras.clear();
+    }
+
     if (master)
     {
         ObjectGuid masterGuid = master->GetObjectGuid();
@@ -692,12 +716,25 @@ void PlayerbotMgr::HandleMasterOutgoingPacket(const WorldPacket& packet)
         bot->GetPlayerbotAI()->HandleMasterOutgoingPacket(packet);
     }
 
+    if (GetMaster()->GetGroup())
+    {
+        Group::MemberSlotList const& groupSlot = GetMaster()->GetGroup()->GetMemberSlots();
+        for (Group::member_citerator itr = groupSlot.begin(); itr != groupSlot.end(); itr++)
+        {
+            Player* bot = sObjectMgr.GetPlayer(itr->guid);
+            if (bot->GetPlayerbotAI()->GetMaster() == GetMaster())
+                bot->GetPlayerbotAI()->HandleMasterOutgoingPacket(packet);
+        }
+    }
+
+    /*
     for (PlayerBotMap::const_iterator it = sRandomPlayerbotMgr.GetPlayerBotsBegin(); it != sRandomPlayerbotMgr.GetPlayerBotsEnd(); ++it)
     {
         Player* const bot = it->second;
         if (bot->GetPlayerbotAI()->GetMaster() == GetMaster())
             bot->GetPlayerbotAI()->HandleMasterOutgoingPacket(packet);
     }
+    */
 }
 
 void PlayerbotMgr::SaveToDB()
