a1cd5152cd91c4e09560dabeed30e0add979a5c8
celguar <celguar@gmail.com>
2021-11-03 04:11:58 +0300

    Fix offering a full guild charter
    Try to register guild/buy tabard only if in city

--- playerbot/strategy/actions/GuildCreateActions.h
+++ playerbot/strategy/actions/GuildCreateActions.h
@@ -26,20 +26,47 @@ namespace ai
     public:
         PetitionOfferNearbyAction(PlayerbotAI* ai) : PetitionOfferAction(ai, "petition offer nearby") {}
         virtual bool Execute(Event event);
-        virtual bool isUseful() { return !bot->GetGuildId() && AI_VALUE2(uint32, "item count", chat->formatQItem(5863)); };
+        virtual bool isUseful() { return !bot->GetGuildId() && AI_VALUE2(uint32, "item count", chat->formatQItem(5863)) && AI_VALUE(uint8, "petition signs") < sWorld.getConfig(CONFIG_UINT32_MIN_PETITION_SIGNS); };
     };
 
     class PetitionTurnInAction : public ChooseTravelTargetAction {
     public:
-        PetitionTurnInAction(PlayerbotAI* ai) : ChooseTravelTargetAction(ai, "petition turn in") {}
+        PetitionTurnInAction(PlayerbotAI* ai) : ChooseTravelTargetAction(ai, "turn in petition") {}
         virtual bool Execute(Event event);
-        virtual bool isUseful() { return !bot->GetGuildId() && AI_VALUE2(uint32, "item count", chat->formatQItem(5863)) && AI_VALUE(uint8, "petition signs") >= sWorld.getConfig(CONFIG_UINT32_MIN_PETITION_SIGNS) && !context->GetValue<TravelTarget*>("travel target")->Get()->isTraveling(); };
+        virtual bool isUseful()
+        {
+            bool inCity = false;
+            AreaTableEntry const* areaEntry = GetAreaEntryByAreaID(bot->GetAreaId());
+            if (areaEntry)
+            {
+                if (areaEntry->zone)
+                    areaEntry = GetAreaEntryByAreaID(areaEntry->zone);
+
+                if (areaEntry && areaEntry->flags & AREA_FLAG_CAPITAL)
+                    inCity = true;
+            }
+
+            return inCity && !bot->GetGuildId() && AI_VALUE2(uint32, "item count", chat->formatQItem(5863)) && AI_VALUE(uint8, "petition signs") >= sWorld.getConfig(CONFIG_UINT32_MIN_PETITION_SIGNS) && !context->GetValue<TravelTarget*>("travel target")->Get()->isTraveling();
+        };
     };
 
     class BuyTabardAction : public ChooseTravelTargetAction {
     public:
         BuyTabardAction(PlayerbotAI* ai) : ChooseTravelTargetAction(ai, "buy tabard") {}
         virtual bool Execute(Event event);
-        virtual bool isUseful() { return bot->GetGuildId() && !AI_VALUE2(uint32, "item count", chat->formatQItem(5976)) && AI_VALUE2(uint32, "free money for", uint32(NeedMoneyFor::guild)) >= 10000 && !context->GetValue<TravelTarget*>("travel target")->Get()->isTraveling(); };
+        virtual bool isUseful()
+        {
+            bool inCity = false;
+            AreaTableEntry const* areaEntry = GetAreaEntryByAreaID(bot->GetAreaId());
+            if (areaEntry)
+            {
+                if (areaEntry->zone)
+                    areaEntry = GetAreaEntryByAreaID(areaEntry->zone);
+
+                if (areaEntry && areaEntry->flags & AREA_FLAG_CAPITAL)
+                    inCity = true;
+            }
+            return inCity && bot->GetGuildId() && !AI_VALUE2(uint32, "item count", chat->formatQItem(5976)) && AI_VALUE2(uint32, "free money for", uint32(NeedMoneyFor::guild)) >= 10000 && !context->GetValue<TravelTarget*>("travel target")->Get()->isTraveling();
+        };
     };
 }
