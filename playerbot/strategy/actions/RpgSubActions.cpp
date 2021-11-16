2c7d0f23b8728f1302dd66af49f83d2fac2ebdc9
celguar <celguar@gmail.com>
2021-11-17 00:37:12 +0300

    Update RpgSubActions.cpp
    Do not make other players face bot when doing rpg

--- playerbot/strategy/actions/RpgSubActions.cpp
+++ playerbot/strategy/actions/RpgSubActions.cpp
@@ -46,6 +46,9 @@ void RpgHelper::setFacing(GuidPosition guidPosition)
     if (!guidPosition.IsUnit())
         return;
 
+    if (guidPosition.IsPlayer())
+        return;
+
     Unit* unit = guidPosition.GetUnit();
 
     unit->SetFacingTo(unit->GetAngle(bot));
