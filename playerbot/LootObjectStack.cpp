ad0b16714583dc74e646549c06cfc92f14cd2b8e
celguar <celguar@gmail.com>
2022-03-26 17:25:34 +0300

    Allow looting some quest objects that have herbalism in code

--- playerbot/LootObjectStack.cpp
+++ playerbot/LootObjectStack.cpp
@@ -85,6 +85,7 @@ void LootObject::Refresh(Player* bot, ObjectGuid guid)
 #endif
         && go->GetGoState() == GO_STATE_READY)
     {
+        uint32 goId = go->GetGOInfo()->id;
         uint32 lockId = go->GetGOInfo()->GetLockId();
         LockEntry const *lockInfo = sLockStore.LookupEntry(lockId);
         if (!lockInfo)
@@ -102,7 +103,11 @@ void LootObject::Refresh(Player* bot, ObjectGuid guid)
                 }
                 break;
             case LOCK_KEY_SKILL:
-                if (SkillByLockType(LockType(lockInfo->Index[i])) > 0)
+                if (goId == 13891 || goId == 19535) // Serpentbloom
+                {
+                    this->guid = guid;
+                }
+                else if (SkillByLockType(LockType(lockInfo->Index[i])) > 0)
                 {
                     skillId = SkillByLockType(LockType(lockInfo->Index[i]));
                     reqSkillValue = max((uint32)1, lockInfo->Skill[i]);
