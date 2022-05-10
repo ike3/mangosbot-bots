d15ff752886c68a471f9274328c7f0aa97c28de2
celguar <celguar@gmail.com>
2022-05-10 15:55:54 +0300

    Update FollowMasterStrategy.cpp

--- playerbot/strategy/generic/FollowMasterStrategy.cpp
+++ playerbot/strategy/generic/FollowMasterStrategy.cpp
@@ -4,11 +4,6 @@
 
 using namespace ai;
 
-//NextAction** FollowMasterStrategy::getDefaultActions()
-//{
-//    return NextAction::array(0, new NextAction("follow", 1.0f), NULL);
-//}
-
 void FollowMasterStrategy::InitTriggers(std::list<TriggerNode*> &triggers)
 {
     triggers.push_back(new TriggerNode(
