0759572c1bea51f2b682783afdd69cedb1b83f3c
mostlikely4r <sebastiaan.keek@gmail.com>
2022-07-18 16:20:20 +0200

    -Bot budget tweak: Bots no longer save up for a tabard (1gold) until they are level 20.

--- playerbot/strategy/values/BudgetValues.cpp
+++ playerbot/strategy/values/BudgetValues.cpp
@@ -181,7 +181,7 @@ uint32 MoneyNeededForValue::Calculate()
     case NeedMoneyFor::guild:
         if (ai->HasStrategy("guild", BOT_STATE_NON_COMBAT))
         {
-            if (bot->GetGuildId())
+            if (bot->GetGuildId() && bot->GetLevel() > 20)
                 moneyWanted = AI_VALUE2(uint32, "item count", chat->formatQItem(5976)) ? 0 : 10000; //1g (tabard)
             else
                 moneyWanted = AI_VALUE2(uint32, "item count", chat->formatQItem(5863)) ? 0 : 10000; //10s (guild charter)
