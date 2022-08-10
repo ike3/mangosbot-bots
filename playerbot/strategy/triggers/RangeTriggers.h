ff338063dd02f06fb02d05c369c56f2d80753786
celguar <celguar@gmail.com>
2022-08-11 02:53:21 +0300

    Increase too close range for ranged classes

--- playerbot/strategy/triggers/RangeTriggers.h
+++ playerbot/strategy/triggers/RangeTriggers.h
@@ -38,16 +38,16 @@ namespace ai
                 if (target->IsPlayer())
                 {
                     if (!isVictim)
-                        coeff = 0.4f;
+                        coeff = 0.7f;
                     else
-                        coeff = 0.8f;
+                        coeff = 1.0f;
                 }
                 else
                 {
                     if (!isVictim)
-                        coeff = 0.3f;
+                        coeff = 0.4f;
                     else
-                        coeff = 0.5f;
+                        coeff = 0.6f;
                 }
 
                 if (isRaid)
@@ -92,16 +92,16 @@ namespace ai
             if (target->IsPlayer())
             {
                 if (!isVictim)
-                    coeff = 0.4f;
+                    coeff = 0.7f;
                 else
-                    coeff = 0.8f;
+                    coeff = 1.0f;
             }
             else
             {
                 if (!isVictim)
-                    coeff = 0.3f;
+                    coeff = 0.4f;
                 else
-                    coeff = 0.5f;
+                    coeff = 0.6f;
             }
 
             if (isRaid)
