721441b0b5fc12264ad8eab0f75ff83df086da04
mostlikely4r <sebastiaan.keek@gmail.com>
2022-08-11 08:56:28 +0200

    -Travel fix: Intempestive assertion fail fix due to pNext == ed (by atidot3)

--- playerbot/TravelNode.cpp
+++ playerbot/TravelNode.cpp
@@ -761,6 +761,10 @@ bool TravelPath::shouldMoveToNextPoint(WorldPosition startPos, vector<PathNodePo
 
     auto nextP = std::next(p);
 
+    // Fix assertion fail due to nextP to be invalidate
+    if (nextP == ed) //We are the end. Stop now.
+        return false;
+
     //We are moving to a area trigger node and want to move to the next teleport node.
     if (p->type == NODE_PORTAL && nextP->type == NODE_PORTAL && p->entry == nextP->entry)
     {
