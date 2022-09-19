19922747d759a7ef98074dfe23bfb73faace50b6
mostlikely4r <sebastiaan.keek@gmail.com>
2022-09-19 14:51:05 +0200

    -Rpg change: Bots now slightly randomize their rpg actions.

--- playerbot/strategy/generic/RpgStrategy.cpp
+++ playerbot/strategy/generic/RpgStrategy.cpp
@@ -12,8 +12,11 @@ float RpgActionMultiplier::GetValue(Action* action)
     string nextAction = AI_VALUE(string, "next rpg action");
     string name = action->getName();
 
-    if (!nextAction.empty() && dynamic_cast<RpgEnabled*>(action) && name != nextAction)
-        return 0.1f;
+    if (dynamic_cast<RpgEnabled*>(action))
+        if (!nextAction.empty() && name != nextAction)
+            return 0.1f;
+        else
+            return frand(0.2f, 1.0f);
 
     return 1.0f;
 }
