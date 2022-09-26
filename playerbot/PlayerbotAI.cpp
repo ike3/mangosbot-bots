23c766b55d8310090903417a13a37c3ce614cdc0
mostlikely4r <sebastiaan.keek@gmail.com>
2022-09-26 19:04:05 +0200

    -Rpg trade fix: Bots will no longer be stuck in trade with other bots after getting the reset command (like after teleport).

--- playerbot/PlayerbotAI.cpp
+++ playerbot/PlayerbotAI.cpp
@@ -482,6 +482,9 @@ void PlayerbotAI::Reset(bool full)
     LastSpellCast & lastSpell = aiObjectContext->GetValue<LastSpellCast& >("last spell cast")->Get();
     lastSpell.Reset();
 
+    if (bot->GetTradeData())
+        bot->TradeCancel(true);
+
     if (full)
     {
         aiObjectContext->GetValue<LastMovement& >("last movement")->Get().Set(NULL);
