2094dd91ab6cbea2d6b254910d1cee2d4c752ccf
davidonete <davidparraausina@gmail.com>
2022-10-25 09:02:50 +0100

    Revert "Fix healers moving back and forth"
    This reverts commit 11baf7dff719ffa908b04fb09c79e7268121bb4b.

--- playerbot/strategy/paladin/HealPaladinStrategy.h
+++ playerbot/strategy/paladin/HealPaladinStrategy.h
@@ -12,6 +12,7 @@ namespace ai
     public:
         virtual void InitTriggers(std::list<TriggerNode*> &triggers);
         virtual string getName() { return "heal"; }
+        virtual NextAction** getDefaultActions();
 		virtual int GetType() { return STRATEGY_TYPE_HEAL | STRATEGY_TYPE_MELEE; }
     };
 }
