54f208cb10f543e306a7f632456f0ff254237708
celguar <celguar@gmail.com>
2022-11-03 02:42:56 +0300

    Less often guild trigger check

--- playerbot/strategy/triggers/GuildTriggers.h
+++ playerbot/strategy/triggers/GuildTriggers.h
@@ -6,7 +6,7 @@ namespace ai
     class PetitionTurnInTrigger : public Trigger {
     public:
         PetitionTurnInTrigger(PlayerbotAI* ai) :
-            Trigger(ai) {}
+            Trigger(ai, "petition turn in trigger", 5) {}
 
         bool IsActive() { return !bot->GetGuildId() && AI_VALUE2(uint32, "item count", chat->formatQItem(5863)) && AI_VALUE(uint8, "petition signs") >= sWorld.getConfig(CONFIG_UINT32_MIN_PETITION_SIGNS); };
     };
@@ -14,7 +14,7 @@ namespace ai
     class BuyTabardTrigger : public Trigger {
     public:
         BuyTabardTrigger(PlayerbotAI* ai) :
-            Trigger(ai) {}
+            Trigger(ai, "buy tabard trigger", 5) {}
 
         bool IsActive();
     };
@@ -22,7 +22,7 @@ namespace ai
     class LeaveLargeGuildTrigger : public Trigger {
     public:
         LeaveLargeGuildTrigger(PlayerbotAI* ai) :
-            Trigger(ai) {}
+            Trigger(ai, "leave large guild trigger", 10) {}
 
         bool IsActive();
     };
