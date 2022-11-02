ce9bcf9146f0ac0cca2caf17bf8beb9e40e3cf24
celguar <celguar@gmail.com>
2022-11-03 02:39:55 +0300

    Possible fix for stealth attacks

--- playerbot/strategy/values/EnemyPlayerValue.cpp
+++ playerbot/strategy/values/EnemyPlayerValue.cpp
@@ -62,8 +62,8 @@ bool NearestEnemyPlayersValue::AcceptUnit(Unit* unit)
         !enemy->HasFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_ATTACKABLE_1) &&
         !enemy->HasFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_UNTARGETABLE) &&
         ((inCannon || !enemy->HasFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_UNINTERACTIBLE))) &&
-        //!enemy->HasStealthAura() &&
-        //!enemy->HasInvisibilityAura() &&
+        !enemy->HasStealthAura() &&
+        !enemy->HasInvisibilityAura() &&
         enemy->IsVisibleForOrDetect(bot, bot->GetCamera().GetBody(), false) &&
         !enemy->HasAuraType(SPELL_AURA_SPIRIT_OF_REDEMPTION)
         );
