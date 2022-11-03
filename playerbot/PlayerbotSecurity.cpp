67344f9bd5f0d648abc1e2899bb4fbb739e155dd
mostlikely4r <sebastiaan.keek@gmail.com>
2022-11-03 16:20:57 +0100

    -Alwaysbot fix: Use same security for always bots as randombots.

--- playerbot/PlayerbotSecurity.cpp
+++ playerbot/PlayerbotSecurity.cpp
@@ -23,7 +23,7 @@ PlayerbotSecurityLevel PlayerbotSecurity::LevelFor(Player* from, DenyReason* rea
         return PlayerbotSecurityLevel::PLAYERBOT_SECURITY_DENY_ALL;
     }
 
-    if (sPlayerbotAIConfig.IsInRandomAccountList(account))
+    if (sPlayerbotAIConfig.IsInRandomAccountList(account) || sPlayerbotAIConfig.IsInNonRandomAccountList(account))
     {
         if (bot->GetPlayerbotAI()->IsOpposing(from))
         {
