a8ca1237ce8e41e0e6b694dc3343c3e140cd0820
mostlikely4r <sebastiaan.keek@gmail.com>
2023-02-10 16:24:40 +0100

    -Help gen: Added rpg strategy help text.

--- playerbot/strategy/generic/RpgStrategy.h
+++ playerbot/strategy/generic/RpgStrategy.h
@@ -25,7 +25,7 @@ namespace ai
             "This is the main rpg strategy which make bots pickand move to various rpg targets.\n"
             "The interactions included in this strategy are limited to emotesand wait.";            
         }
-        virtual vector<string> GetRelatedStrategies() { return {"rpg quest", "rpg vendor", "rpg explore", "rpg maintenance", "rpg guild", "rpg bg", "rpg player", "rpg craft"}; }
+        virtual vector<string> GetRelatedStrategies() { return {"rpg quest", "rpg vendor", "rpg explore", "rpg maintenance", "rpg guild", "rpg bg", "rpg player", "rpg craft", "debug rpg"}; }
 #endif
     protected:
         virtual void InitNonCombatTriggers(std::list<TriggerNode*> &triggers) override;
@@ -37,7 +37,15 @@ namespace ai
     public:
         RpgQuestStrategy(PlayerbotAI* ai) : RpgStrategy(ai) {};
         string getName() override { return "rpg quest"; }
-
+#ifdef GenerateBotHelp
+        virtual string GetHelpName() { return "rpg quest"; } //Must equal iternal name
+        virtual string GetHelpDescription() {
+            return "This strategy makes bot move to npcs that have quests the bot can pick up or hand in.\n"
+                "Bots will only pick up quests that give xp unless low on durability.\n"
+                "Bots will also try to do repeatable quests.";
+        }
+        virtual vector<string> GetRelatedStrategies() { return { "rpg" }; }
+#endif
     private:
         void InitNonCombatTriggers(std::list<TriggerNode*>& triggers) override;
     };
@@ -47,7 +55,15 @@ namespace ai
     public:
         RpgVendorStrategy(PlayerbotAI* ai) : RpgStrategy(ai) {};
         string getName() override { return "rpg vendor"; }
-
+#ifdef GenerateBotHelp
+        virtual string GetHelpName() { return "rpg vendor"; } //Must equal iternal name
+        virtual string GetHelpDescription() {
+            return "This strategy makes bot move to vendors, auctionhouses and mail-boxes.\n"
+                "Bots will buy and sell items depending on usage see [h:action|query item usage].\n"
+                "Bots will pick up their mail if there is any items or gold attached.";
+        }
+        virtual vector<string> GetRelatedStrategies() { return { "rpg" }; }
+#endif
     private:
         virtual void InitNonCombatTriggers(std::list<TriggerNode*>& triggers) override;
     };
@@ -57,7 +73,15 @@ namespace ai
     public:
         RpgExploreStrategy(PlayerbotAI* ai) : RpgStrategy(ai) {};
         string getName() override { return "rpg explore"; }
-
+#ifdef GenerateBotHelp
+        virtual string GetHelpName() { return "rpg explore"; } //Must equal iternal name
+        virtual string GetHelpDescription() {
+            return "This strategy makes bot move to innkeepers and flight masters.\n"
+                "Bots will make this inn their new home when their current home is far away.\n"
+                "Bots pick a random flight (when not with a player) or discover new flightpaths.";
+        }
+        virtual vector<string> GetRelatedStrategies() { return { "rpg" }; }
+#endif
     private:
         virtual void InitNonCombatTriggers(std::list<TriggerNode*>& triggers) override;
     };
@@ -67,7 +91,15 @@ namespace ai
     public:
         RpgMaintenanceStrategy(PlayerbotAI* ai) : RpgStrategy(ai) {};
         string getName() override { return "rpg maintenance"; }
-
+#ifdef GenerateBotHelp
+        virtual string GetHelpName() { return "rpg maintenance"; } //Must equal iternal name
+        virtual string GetHelpDescription() {
+            return "This strategy makes bot move to trainers and armorers.\n"
+                "Bots will automatically train new spells or skills that are available.\n"
+                "Bots will automatically repair their armor when below 80% durability.";
+        }
+        virtual vector<string> GetRelatedStrategies() { return { "rpg" }; }
+#endif
     private:
         virtual void InitNonCombatTriggers(std::list<TriggerNode*>& triggers) override;
     };
@@ -77,7 +109,14 @@ namespace ai
     public:
         RpgGuildStrategy(PlayerbotAI* ai) : RpgStrategy(ai) {};
         string getName() override { return "rpg guild"; }
-
+#ifdef GenerateBotHelp
+        virtual string GetHelpName() { return "rpg guild"; } //Must equal iternal name
+        virtual string GetHelpDescription() {
+            return "This strategy makes bot move to guild master npcs.\n"
+                "Bots will automatically buy a petition if they are not already in a guild.";
+        }
+        virtual vector<string> GetRelatedStrategies() { return { "rpg" }; }
+#endif
     private:
         virtual void InitNonCombatTriggers(std::list<TriggerNode*>& triggers) override;
     };
@@ -87,7 +126,14 @@ namespace ai
     public:
         RpgBgStrategy(PlayerbotAI* ai) : RpgStrategy(ai) {};
         string getName() override { return "rpg bg"; }
-
+#ifdef GenerateBotHelp
+        virtual string GetHelpName() { return "rpg bg"; } //Must equal iternal name
+        virtual string GetHelpDescription() {
+            return "This strategy makes bot move to battlemasters.\n"
+                "Bots will automatically queue for battlegrounds.";
+        }
+        virtual vector<string> GetRelatedStrategies() { return { "rpg" }; }
+#endif
     private:
         virtual void InitNonCombatTriggers(std::list<TriggerNode*>& triggers) override;
     };
@@ -97,7 +143,15 @@ namespace ai
     public:
         RpgPlayerStrategy(PlayerbotAI* ai) : RpgStrategy(ai) {};
         string getName() override { return "rpg player"; }
-
+#ifdef GenerateBotHelp
+        virtual string GetHelpName() { return "rpg player"; } //Must equal iternal name
+        virtual string GetHelpDescription() {
+            return "This strategy makes bot move to nearby players.\n"
+                "Bots will automatically trade other bots items they don't need but the other bot can use.\n"
+                "Bots will automatically start duels with other players 3 levels above and 10 levels below them.";
+        }
+        virtual vector<string> GetRelatedStrategies() { return { "rpg" }; }
+#endif
     private:
         virtual void InitNonCombatTriggers(std::list<TriggerNode*>& triggers) override;
     };
@@ -107,7 +161,15 @@ namespace ai
     public:
         RpgCraftStrategy(PlayerbotAI* ai) : RpgStrategy(ai) {};
         string getName() override { return "rpg craft"; }
-
+#ifdef GenerateBotHelp
+        virtual string GetHelpName() { return "rpg craft"; } //Must equal iternal name
+        virtual string GetHelpDescription() {
+            return "This strategy makes bot move to nearby objects, players, npcs.\n"
+                "Bots will automatically craft items they think are usefull or will give them skill points.\n"
+                "Bots will automatically cast random spells on or near players or npcs.";
+        }
+        virtual vector<string> GetRelatedStrategies() { return { "rpg" }; }
+#endif
     private:
         virtual void InitNonCombatTriggers(std::list<TriggerNode*>& triggers) override;
     };
