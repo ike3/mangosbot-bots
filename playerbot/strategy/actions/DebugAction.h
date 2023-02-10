e59b2eb8bd2549bfa01f0420b8f6681f1046f79a
mostlikely4r <sebastiaan.keek@gmail.com>
2023-02-10 16:23:37 +0100

    -Internal: Removed capital D from Debug.

--- playerbot/strategy/actions/DebugAction.h
+++ playerbot/strategy/actions/DebugAction.h
@@ -6,7 +6,7 @@ namespace ai
     class DebugAction : public ChatCommandAction
     {
     public:
-        DebugAction(PlayerbotAI* ai) : ChatCommandAction(ai, "Debug") {}
+        DebugAction(PlayerbotAI* ai) : ChatCommandAction(ai, "debug") {}
         virtual bool Execute(Event& event) override;
         void FakeSpell(uint32 spellId, Unit* truecaster, Unit* caster, ObjectGuid target = ObjectGuid(), list<ObjectGuid> otherTargets = {}, list<ObjectGuid> missTargets = {}, WorldPosition source = WorldPosition(), WorldPosition dest = WorldPosition(), bool forceDest = false);
         void addAura(uint32 spellId, Unit* target);
