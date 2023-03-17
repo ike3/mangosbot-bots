97d891fc59d55eeb54dd502444cd16e4fabd3e24
mostlikely4r <sebastiaan.keek@gmail.com>
2023-03-17 16:20:50 +0100

    -Rpg fix: Bots now are less likely to do useless rpg actions.

--- playerbot/strategy/generic/RpgStrategy.cpp
+++ playerbot/strategy/generic/RpgStrategy.cpp
@@ -17,7 +17,7 @@ float RpgActionMultiplier::GetValue(Action* action)
         if (!nextAction.empty() && name != nextAction)
             return 0.1f;
         else
-            return frand(0.2f, 1.0f);
+            return frand(0.8f, 1.0f);
     }
 
     return 1.0f;
