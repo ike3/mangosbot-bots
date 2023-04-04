503a7fc7379ef40f71980ddaf8a0ce8092aa014d
David <davidparraausina@gmail.com>
2023-04-04 09:46:13 +0100

    Reset bot raid ids when invited to a group

--- playerbot/strategy/actions/ResetInstancesAction.cpp
+++ playerbot/strategy/actions/ResetInstancesAction.cpp
@@ -2,7 +2,6 @@
 #include "../../playerbot.h"
 #include "ResetInstancesAction.h"
 
-
 using namespace ai;
 
 bool ResetInstancesAction::Execute(Event& event)
@@ -11,5 +10,19 @@ bool ResetInstancesAction::Execute(Event& event)
     bot->GetSession()->HandleResetInstancesOpcode(packet);
 
     ai->TellMaster("Resetting all instances", PlayerbotSecurityLevel::PLAYERBOT_SECURITY_ALLOW_ALL, false);
+    return true;
+}
+
+bool ResetRaidsAction::Execute(Event& event)
+{
+    Player::BoundInstancesMap& binds = bot->GetBoundInstances();
+    for (Player::BoundInstancesMap::iterator itr = binds.begin(); itr != binds.end(); ++itr)
+    {
+        if (itr->first != bot->GetMapId())
+        {
+            bot->UnbindInstance(itr);
+        }
+    }
+
     return true;
 }
