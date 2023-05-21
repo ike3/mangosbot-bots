84b23cabe2e4b9ca84189f22f67382d30366142a
mostlikely4r <sebastiaan.keek@gmail.com>
2023-05-21 20:07:19 +0200

    -Internal: Possible memory leak when destroying values.

--- playerbot/strategy/Value.h
+++ playerbot/strategy/Value.h
@@ -13,6 +13,7 @@ namespace ai
     {
     public:
         UntypedValue(PlayerbotAI* ai, string name) : AiNamedObject(ai, name) {}
+        virtual ~UntypedValue() = default;
         virtual void Update() {}  //Nonfunctional see AiObjectContext::Update() to enable.
         virtual void Reset() {}
         virtual string Format() { return "?"; }
