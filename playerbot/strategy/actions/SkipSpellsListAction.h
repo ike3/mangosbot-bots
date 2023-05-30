dea80ea8259ba0fc6107726209a9de9c17444a8f
mostlikely4r <sebastiaan.keek@gmail.com>
2023-05-30 21:48:46 +0200

    -Help: Added help for skip spells list.

--- playerbot/strategy/actions/SkipSpellsListAction.h
+++ playerbot/strategy/actions/SkipSpellsListAction.h
@@ -7,8 +7,22 @@ namespace ai
     class SkipSpellsListAction : public ChatCommandAction
     {
     public:
-        SkipSpellsListAction(PlayerbotAI* ai) : ChatCommandAction(ai, "ss") {}
+        SkipSpellsListAction(PlayerbotAI* ai) : ChatCommandAction(ai, "skip spells list") {}
         virtual bool Execute(Event& event) override;
+
+#ifdef GenerateBotHelp
+        virtual string GetHelpName() { return "skip spells list"; } //Must equal iternal name
+        virtual string GetHelpDescription()
+        {
+            return "This chat command gives control over the list of spells bots aren't allowed to cast.\n"
+                "Examples:\n"
+                "ss ? : List the ignored spells.\n"
+                "ss [spell name] : Never cast this spell.\n"
+                "ss -[spell name] : Remove this spell from the ignored spell list.\n";
+        }
+        virtual vector<string> GetUsedActions() { return {}; }
+        virtual vector<string> GetUsedValues() { return { "skip spells list" }; }
+#endif 
 
     private:
         SpellIds parseIds(string text);
