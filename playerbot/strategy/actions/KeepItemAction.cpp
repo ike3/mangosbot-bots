bad02103af37cc9bf3d8e236c94d44285e759a3a
mostlikely4r <sebastiaan.keek@gmail.com>
2023-07-05 09:20:45 +0200

    -Keep fix: Allow using item links which the bot doesn't own to set keep.

--- playerbot/strategy/actions/KeepItemAction.cpp
+++ playerbot/strategy/actions/KeepItemAction.cpp
@@ -25,11 +25,20 @@ bool KeepItemAction::Execute(Event& event)
     else if(text.find(" ") != string::npos) //Remove type from param.
         text = text.substr(text.find(" ") + 1);
 
-    IterateItemsMask mask = IterateItemsMask((uint8)IterateItemsMask::ITERATE_ITEMS_IN_EQUIP | (uint8)IterateItemsMask::ITERATE_ITEMS_IN_BAGS | (uint8)IterateItemsMask::ITERATE_ITEMS_IN_BANK);
+    ItemIds ids = chat->parseItems(text);
 
-    list<Item*> found = ai->InventoryParseItems(text, mask);
+    if (ids.empty())
+    {
+
+        IterateItemsMask mask = IterateItemsMask((uint8)IterateItemsMask::ITERATE_ITEMS_IN_EQUIP | (uint8)IterateItemsMask::ITERATE_ITEMS_IN_BAGS | (uint8)IterateItemsMask::ITERATE_ITEMS_IN_BANK);
+
+        list<Item*> found = ai->InventoryParseItems(text, mask);
+
+        for (auto& item : found)
+            ids.insert(item->GetProto()->ItemId);
+    }
    
-    if (found.empty())
+    if (ids.empty())
     {
         ostringstream out;
 
@@ -44,16 +53,11 @@ bool KeepItemAction::Execute(Event& event)
 
     if (type == "?")
     {
-        for (auto& item : found)
+        for (auto& id : ids)
         {
-            const ItemPrototype* proto = item->GetProto();
-
-            if (!proto)
-                continue;
-
             ostringstream out;
-            out << chat->formatItem(proto);
-            out << ": " << keepName[AI_VALUE2_EXISTS(ForceItemUsage, "force item usage", proto->ItemId, ForceItemUsage::FORCE_USAGE_NEED)] << " the item.";
+            out << chat->formatQItem(id);
+            out << ": " << keepName[AI_VALUE2_EXISTS(ForceItemUsage, "force item usage", id, ForceItemUsage::FORCE_USAGE_NEED)] << " the item.";
             ai->TellPlayer(GetMaster(), out.str());
 
         }
@@ -72,12 +76,12 @@ bool KeepItemAction::Execute(Event& event)
     else if (type == "need")
         usage = ForceItemUsage::FORCE_USAGE_NEED;
 
-    for (auto& item : found)
+    for (auto& id : ids)
     {
-        if (AI_VALUE2_EXISTS(ForceItemUsage, "force item usage", item->GetEntry(), ForceItemUsage::FORCE_USAGE_NONE) != usage)
+        if (AI_VALUE2_EXISTS(ForceItemUsage, "force item usage", id, ForceItemUsage::FORCE_USAGE_NONE) != usage)
         {
             changed++;
-            SET_AI_VALUE2(ForceItemUsage, "force item usage", item->GetEntry(), usage);
+            SET_AI_VALUE2(ForceItemUsage, "force item usage", id, usage);
         }
     }
 
